d196cf182bf633d229bebcc77a6693b960c1fcc1
Willian Marques <willian.marques0724@jala.university>
2026-04-28 11:42:05 -0300

    Forca recarga de assets no navegador

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"></script>
-<script src="<?= url('assets/app.js') ?>"></script>
+<?php $appJsPath = __DIR__ . '/../public/assets/app.js'; ?>
+<script src="<?= url('assets/app.js?v=' . (is_file($appJsPath) ? filemtime($appJsPath) : time())) ?>"></script>
 </body>
 </html>
-
--- views/header.php
+++ views/header.php
@@ -6,7 +6,7 @@
     <title><?= e($pageTitle ?? 'Controle de Processos') ?> | <?= e(config('app_name')) ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?= url('assets/styles.css') ?>">
+    <?php $stylesPath = __DIR__ . '/../public/assets/styles.css'; ?>
+    <link rel="stylesheet" href="<?= url('assets/styles.css?v=' . (is_file($stylesPath) ? filemtime($stylesPath) : time())) ?>">
 </head>
 <body class="<?= e($bodyClass ?? 'app-body') ?>">
-
